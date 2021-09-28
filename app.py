08dcf07c76c33d92948e1505ee3b72e8e14c1531
HarshitPathak009 <harshitpathak009@gmail.com>
2021-09-28 20:12:20 +0530

    Troops Cannons. ToDo-> add attack of this

--- app.py
+++ app.py
@@ -16,59 +16,66 @@ class Characteristics:
     
     def move(self,board):
         if(self.color == BLUE):
-            distance = self.movementSpeed * 1
+            distance = self.movementDistance * 1
         else:
-            distance = self.movementSpeed * -1
-        if((self.y+distance<8 and self.y+distance>=0) and (board[self.x][self.y+distance]==0 or board[self.x][self.y+distance].cost==0)):
+            distance = self.movementDistance * -1
+        if((self.y+distance<8 and self.y+distance>=0) and (board[self.x][self.y+distance]==0 or board[self.x][self.y+distance].health==0)):
             board[self.x][self.y] = 0
             board[self.x][self.y+distance] = self
             self.y = self.y+distance
 
 
-# class Troop(Characteristics):
-#     def __init__(*p):
-#         Characteristics.__init__(p)
+class Troop(Characteristics):
+    def __init__(self, x, y, color):
+        super().__init__(x,y, color)
+        self.damage = 1
+        self.attackDistance = 0
+        self.movementDistance = 2
+        self.cost = TROOP_COST
+        self.health = TROOP_COST
+        self.isMovable = True
     
-#     #attack
-#     def attack(self):
-#         block = 1
+    def attack(self,board):
+        pass
 
         
-#     #move
+    #move
 
-#     #health
+    #health
 
-#     #destroy
+    #destroy
 
 
-# class Cannon(Characteristics):
-#     def __init__(self, *p):
-#         print(p)
-#         self.x = p[0]
-#         self.y = p[1]
-#         Characteristics.__init__(p)
+class Cannon(Characteristics):
+    def __init__(self, x,y,color):
+        super().__init__(x,y, color)
+        self.damage = 1
+        self.attackDistance = 0
+        self.movementDistance = 0
+        self.cost = CANNON_COST
+        self.health = CANNON_COST
+        self.isMovable = False
     
-#     #attack
-#     def attack():
-#         block = 1
+    #attack
+    def attack(self, board):
+        pass
         
-#     #move
+    #move
 
-#     #health
+    #health
 
-#     #destroy
+    #destroy
 
 
 class Archer(Characteristics):
     def __init__(self, x, y, color):
         super().__init__(x,y, color)
         self.damage = 1
-        self.attackSpeed = 0
-        self.movementSpeed = 0
+        self.attackDistance = 0
+        self.movementDistance = 1
         self.cost = ARCHER_COST
-        self.health = ARCHER_HEALTH
+        self.health = ARCHER_COST
         self.isMovable = True
-        self.isDestroyed = False
         
 
     
@@ -77,62 +84,76 @@ class Archer(Characteristics):
         #ToDo-> add the concept of ismoving to void attacking when player moves
         if(self.y+1<8 and self.color==BLUE and board[self.x][self.y+1]!=0):
             if(board[self.x][self.y+1].color == RED):
-                if(board[self.x][self.y+1].cost<=0):
-                    board[self.x][self.y+1].cost=0
+                if(board[self.x][self.y+1].health<=0):
+                    board[self.x][self.y+1].health=0
                 else:
-                    board[self.x][self.y+1].cost-=1
+                    board[self.x][self.y+1].health-=1
         if(self.y-1>=0 and self.color==RED and board[self.x][self.y-1]!=0):
             if(board[self.x][self.y-1].color == BLUE):
-                if(board[self.x][self.y-1].cost<=0):
-                    board[self.x][self.y-1].cost=0
+                if(board[self.x][self.y-1].health<=0):
+                    board[self.x][self.y-1].health=0
                 else:
-                    board[self.x][self.y-1].cost-=1
+                    board[self.x][self.y-1].health-=1
         
 
 def print_board(board):
     for i in range(8):
         for j in range(8):
             if(board[i][j]!=0):
-                print(board[i][j].cost,end="\t")
+                print(board[i][j].health,end="\t")
                 continue
             print(board[i][j],end="\t")
         print("")
 
-def is_correct_board(board):
-    cost = 0
+def Winner(board):
+    remainingBlueHealth = 0
+    remainingRedHealth = 0
 
     #left-side cost
     for i in range(len(board)):
-        for j in range(len(board)//2):
+        for j in range(len(board)):
             if(board[i][j]!=0):
-                cost+=board[i][j].cost
+                if(board[i][j].color == RED):
+                    remainingRedHealth+=board[i][j].health
+                else:
+                    remainingBlueHealth+=board[i][j].health
     # print(cost)
-    if(cost!=100):
-        return False
+    # print(remainingRedHealth)
+    # print(remainingBlueHealth)
+    if(remainingRedHealth>remainingBlueHealth):
+        print("TEAM RED WINS")
+    elif(remainingBlueHealth>remainingRedHealth):
+        print("TEAM BLUE WINS")
+    else:
+        print("IT's a DRAW")
 
-    #right-side cost
-    cost = 0
-    for i in range(len(board)):
-        for j in range((len(board)//2), len(board)):
-            if(board[i][j]!=0):
-                cost+=board[i][j].cost
-    # print(cost)
-    if(cost!=100):
-        return False
-    
-
-    return True
 
 def set_board(board):
     #set Team Blue
     cost = 0
     print("Team Blue")
     while(True):
+        name = int(input("Enter 1 for Troops, 2 for Archer, 3 for Cannon"))
+        if(name>3 or name<=0):
+            print("Please enter the given options")
+            continue
         x, y = map(int,input("Enter the position of character").strip().split())
-        if(y<=3 and board[x][y]==0 and cost+ARCHER_COST<=TEAM_BLUE):
-            a = Archer(x,y,BLUE)
-            board[x][y] = a
-            cost+= ARCHER_COST
+        if(name == 1):
+            if(y<=3 and board[x][y]==0 and cost+TROOP_COST<=TEAM_BLUE):
+                a = Troop(x,y,BLUE)
+                board[x][y] = a
+                cost+= TROOP_COST
+        elif(name ==2):
+            if(y<=3 and board[x][y]==0 and cost+ARCHER_COST<=TEAM_BLUE):
+                a = Archer(x,y,BLUE)
+                board[x][y] = a
+                cost+= ARCHER_COST
+        else:
+            if(y<=3 and board[x][y]==0 and cost+CANNON_COST<=TEAM_BLUE):
+                a = Cannon(x,y,BLUE)
+                board[x][y] = a
+                cost+= CANNON_COST
+        print("Remaining Cost = ", TEAM_BLUE-cost)
         if(cost == TEAM_BLUE):
             break
 
@@ -140,14 +161,28 @@ def set_board(board):
     cost = 0
     print("Team Red")
     while(True):
+        name = int(input("Enter 1 for Troops, 2 for Archer, 3 for Cannon"))
+        if(name>3 or name<=0):
+            print("Please enter the given options")
+            continue
         x, y = map(int,input("Enter the position of character").strip().split())
-        print(y)
-        if(y>=4 and board[x][y]==0 and cost+ARCHER_COST<=TEAM_BLUE):
-            print("In if")
-            a = Archer(x,y,RED)
-            board[x][y] = a
-            cost+= ARCHER_COST
-        if(cost == TEAM_BLUE):
+        if(name == 1):
+            if(y>3 and board[x][y]==0 and cost+TROOP_COST<=TEAM_RED):
+                a = Troop(x,y,RED)
+                board[x][y] = a
+                cost+= TROOP_COST
+        elif(name ==2):
+            if(y>3 and board[x][y]==0 and cost+ARCHER_COST<=TEAM_RED):
+                a = Archer(x,y,RED)
+                board[x][y] = a
+                cost+= ARCHER_COST
+        else:
+            if(y>3 and board[x][y]==0 and cost+CANNON_COST<=TEAM_RED):
+                a = Cannon(x,y,RED)
+                board[x][y] = a
+                cost+= CANNON_COST
+        print("Remaining Cost = ", TEAM_RED-cost)
+        if(cost == TEAM_RED):
             break
 
 def main():
@@ -169,22 +204,20 @@ def main():
 
     #ToDO -> Player positions setting
     moves = 0
-    if(is_correct_board(board)):
-        while(time.time() < endTime):
-            os.system('cls' if os.name == 'nt' else 'clear')
-            print(moves)
-            for i in range(8):
-                for j in range(8):
-                    if(board[i][j]!=0):
-                        board[i][j].attack(board)
-                        if(board[i][j].isMovable == True):
-                            board[i][j].move(board)
-            print_board(board)
-            moves+=1
-            time.sleep(1)
-            
-    else:
-        print("Wrong Configuration")
+    # if(is_correct_board(board)):
+    while(time.time() < endTime):
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(moves)
+        for i in range(8):
+            for j in range(8):
+                if(board[i][j]!=0):
+                    board[i][j].attack(board)
+                    if(board[i][j].isMovable == True):
+                        board[i][j].move(board)
+        print_board(board)
+        moves+=1
+        time.sleep(1)
+    Winner(board)
     
     
 if __name__ == "__main__":
